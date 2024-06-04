601e80e0b61d1092e0614bcc9cf588be7ee5e20d
Alan <alan80perez@gmail.com>
2024-06-04 15:31:20 -0400

    reayd

---
+++ FactoryExample/FactoryExample/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using FactoryExample;
+
+internal class Program
+{
+
+    private static string[] tiposEmpleado = ["Empleado administrativo", "Empleado operativo", "Empleado gerencial"];
+    private static void Main(string[] args)
+    {
+        string[] registerMenu = ["Mostrar empleados", "Registrar empleado", "Borrar empleado"];
+        int opt;
+
+        separator();
+        Console.WriteLine("Bienvenido al gestor de nomina");
+        separator();
+        Console.WriteLine();
+
+        do
+        {
+            separator();
+            Console.WriteLine("Menu principal: ");
+            opt = Utilities.generarOpcionesYResponse(registerMenu);
+            switch (opt)
+            {
+                case 0:MostrarEmpleados();break;
+                case 1:CrearEmpleado();break;
+                case 2:EliminarEmpleado();break;;
+            }
+
+        } while (opt >= 0 && opt < registerMenu.Length);
+
+
+        Console.WriteLine();
+
+        Console.WriteLine("¡Gracias por su visita..!");
+
+    }
+
+
+    private static void CrearEmpleado()
+    {
+        Console.WriteLine("Seleccione el tipo de empleado que desea registrar: ");
+        Console.WriteLine();
+        int opt;
+        do
+        {
+            opt = Utilities.generarOpcionesYResponse(tiposEmpleado);
+            //if(opt ==0)
+
+        } while (opt >= 0 && opt < tiposEmpleado.Length);
+
+        Console.WriteLine();
+    }
+
+    private static void MostrarEmpleados()
+    {
+        Console.WriteLine("ss");
+    }
+
+    private static void EliminarEmpleado()
+    {
+        Console.WriteLine("asasa");
+    }
+
+
+
+
+    private static void separator()
+    {
+        Console.WriteLine("------------------------------------------------");
+    }
+}
---
+++ FactoryExample/FactoryExample/Utilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryExample
+{
+    internal static class Utilities 
+    {
+        public static int generarOpcionesYResponse(string[] options) {
+            int opt;
+            bool successInp;
+            Console.WriteLine();
+            Console.WriteLine("Opciones");
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}) {options[i]}");
+            }
+            Console.WriteLine("Otra tecla) Cancelar");
+            Console.WriteLine();
+            Console.Write("Seleccione la accion que desea realizar: ");
+            successInp = int.TryParse(Console.ReadLine(), out opt);
+            Console.WriteLine();
+            if (!successInp) opt = -1; 
+            return opt -1;
+        }
+    }
+}
